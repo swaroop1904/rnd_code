08cde21525a4fbcd8a8e3b7bcd329cd1374e517c
Swaroop1904 <swaroop1904@gmail.com>
2018-12-09 13:30:13 +0100

    completed the code for action recognition node

--- src/action_recognition/scripts/action_recognition.py
+++ src/action_recognition/scripts/action_recognition.py
@@ -3,14 +3,14 @@
 from keras.models import model_from_json
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import rospkg
+from std_msgs.msg import Int16
 
-# import h5py
+import rospkg
 import rospy
 
 class ActionRecognition(object):
 
-    def __init__(self, web_cam_topic):
+    def __init__(self, web_cam_topic, action_detected_topic):
         self.bridge = CvBridge()
         self.frames = []
         self.count = 0
@@ -18,34 +18,39 @@ class ActionRecognition(object):
         self.width = 306
         self.depth = 10
 
+        # retrieve the action recognition package path
         rospack = rospkg.RosPack()
         package_path = rospack.get_path('action_recognition')
-        print(package_path)
+
+        # load the model architecture and the model weights
         json_file = open(package_path+'/resources/action_3dcnnmodel.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         self.model = model_from_json(loaded_model_json)
-        # load weights into new model
         self.model.load_weights(package_path+"/resources/action_3dcnnmodel-gpu.hd5")
-        print("Loaded model from disk")
-        #self.model.compile(loss=categorical_crossentropy, optimizer=Adam(lr=0.003), metrics=['accuracy'])
-        rospy.Subscriber(web_cam_topic, Image, self.action_recognition_callback)
 
-    def action_recognition_callback(data):
-        data = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+        print("Loaded model from disk")
+
+        # define the subscriber and publisher
+        rospy.Subscriber(web_cam_topic, Image, self.action_recognition_callback)
+        self.detected_action_pub = rospy.Publisher(action_detected_topic, Int16, queue_size=10)
+
+    def action_recognition_callback(self, data):
+        data = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
         data = cv2.resize(data, (self.height, self.width))
         self.frames.append(data)
         if self.count < self.depth - 1:
             self.count += 1
-            return -1
+            self.detected_action_pub.publish(-1)
         else:
             action_class = self.model.predict_classes(self.frames)
             self.count = 0
             self.frames = []
-            return action_class
+            self.detected_action_pub.publish(action_class)
 
 if __name__ == '__main__':
     rospy.init_node('action_recognition_node')
     camera_topic = rospy.get_param('~camera_topic')
-    action_recognition = ActionRecognition(camera_topic)
+    action_detected_topic = rospy.get_param('~action_detected_topic')
+    action_recognition = ActionRecognition(camera_topic, action_detected_topic)
     rospy.spin()
