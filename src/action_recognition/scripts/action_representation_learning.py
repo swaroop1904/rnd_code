e500dad896213c20207eb8cd88bc1648e51a4438
Swaroop1904 <swaroop1904@gmail.com>
2018-12-19 11:59:46 +0100

    changes to account for varied number of steps

--- src/action_recognition/scripts/action_representation_learning.py
+++ src/action_recognition/scripts/action_representation_learning.py
@@ -9,45 +9,127 @@ from action_recognition.msg import Matrix
 class ActionRepresentationLearning(object):
 
     def __init__(self, action_recognition_topic):
-        self.constraints = np.zeros([8,8])
         self.action_recognized_cb = rospy.Subscriber(action_recognition_topic, Int16, self.action_recognized_cb)
         self.action_sequence = []
         self.first_demo = True
-        self.current_demo_constraints = np.zeros([8,8])
+
+        #self.constraints = np.zeros([8,8])
+        self.constraints = np.zeros([1,1])
+        #self.current_demo_constraints = np.zeros([8,8])
+        self.current_demo_constraints = np.zeros([1,1])
+        self.action_map = {}
+        self.current_demo_action_map = {}
+        self.current_demo_action_count = {}
 
         self.constraint_publisher = rospy.Publisher("constraint_topic", Matrix, queue_size=10)
 
 
     def action_recognized_cb(self, action):
-        # -1 is sent only when the demonstration is complete.
         if action.data == -1:
-            self.update_constraints()
-            self.action_sequence = []
-            self.current_demo_constraints = np.zeros([8,8])
-            self.first_demo = False
-            print self.constraints
-            one_dimension_constraints = np.squeeze(self.constraints.reshape([1,64]))
-            self.constraint_publisher.publish(one_dimension_constraints)
-
-        else:
             if self.first_demo:
-                for previous_actions in self.action_sequence:
-                    self.constraints[action.data, previous_actions] = 1
+                self.constraints = self.build_constraint_matrix()
             else:
-                for previous_actions in self.action_sequence:
-                    self.current_demo_constraints[action.data, previous_actions] = 1
-            self.action_sequence.append(action.data)
+                self.current_demo_constraints = self.build_constraint_matrix()
+                self.update_constraints()
+
+            self.first_demo = False
+            self.action_sequence = []
+            self.current_demo_action_map = {}
+            self.current_demo_action_count = {}
+            print self.constraints
+            self.constraint_publisher.publish(self.constraints.flatten())
+        else:
+            action_name = str(action.data)
+            if self.first_demo:
+                if action.data in self.current_demo_action_count:
+                    action_count = self.current_demo_action_count[action.data]
+                    action_name = '{0}_{1}'.format(action.data, action_count)
+                    self.action_map[len(self.action_sequence)] = action.data
+                    self.current_demo_action_count[action.data] += 1
+                else:
+                    self.action_map[len(self.action_sequence)] = action_name
+                    self.current_demo_action_count[action.data] = 1
+            else:
+                if action.data in self.current_demo_action_count:
+                    action_count = self.current_demo_action_count[action.data]
+                    action_name = '{0}_{1}'.format(action.data, action_count)
+                    self.current_demo_action_map[len(self.action_sequence)] = action_name
+                    self.current_demo_action_count[action.data] += 1
+                else:
+                    self.current_demo_action_map[len(self.action_sequence)] = action_name
+                    self.current_demo_action_count[action.data] = 1
+            self.action_sequence.append(action_name)
+
+    def build_constraint_matrix(self):
+        constraint_matrix = np.zeros([len(self.action_sequence), len(self.action_sequence)])
+        for i in range(len(self.action_sequence)):
+            constraint_matrix[i,:i] = 1
+        return constraint_matrix
+
+        # -1 is sent only when the demonstration is complete.
+        # if action.data == -1:
+        #     self.update_constraints()
+        #     self.action_sequence = []
+        #     self.current_demo_constraints = np.zeros([8,8])
+        #     self.first_demo = False
+        #     print self.constraints
+        #     one_dimension_constraints = np.squeeze(self.constraints.reshape([1,64]))
+        #     self.constraint_publisher.publish(one_dimension_constraints)
+        #
+        # else:
+        #     if self.first_demo:
+        #         constraint_new = np.zeros([self.constraints.shape + 1])
+        #         for previous_actions in self.action_sequence:
+        #             constraints_new[action.data, previous_actions] = 1
+        #         self.action_map[len(self.action_sequence)] = action.data
+        #     else:
+        #         for previous_actions in self.action_sequence:
+        #             self.current_demo_constraints[action.data, previous_actions] = 1
+        #     self.action_sequence.append(action.data)
 
     def update_constraints(self):
         # print "**************************************************"
-        if not self.first_demo:
-            conflicting_constraints = self.constraints - self.current_demo_constraints
-            # print self.constraints
-            # print self.current_demo_constraints
-            conflicting_constraints_index = np.where(np.abs(conflicting_constraints) == 1)
-            for diff_index in zip(conflicting_constraints_index[0], conflicting_constraints_index[1]):
-                self.constraints[diff_index] = 0
-            # print self.constraints
+        new_action_list = [action for action in self.action_sequence
+                           if action not in self.action_map.values()]
+
+        prev_action_count = len(self.action_map.values())
+        for action in new_action_list:
+            self.action_map[prev_action_count] = action
+            prev_action_count += 1
+
+        new_action_count = len(self.action_map.values())
+        if new_action_count > 0:
+            new_constraint_matrix = np.zeros((new_action_count, new_action_count))
+            new_constraint_matrix[0:self.constraints.shape[0], 0:self.constraints.shape[1]] = self.constraints
+            self.constraints = new_constraint_matrix
+
+        for i in range(self.current_demo_constraints.shape[0]):
+            action1 = self.current_demo_action_map[i]
+            for j in range(self.current_demo_constraints.shape[1]):
+                action2 = self.current_demo_action_map[j]
+
+                action1_idx = self.get_action_idx(action1)
+                action2_idx = self.get_action_idx(action2)
+                prev_constraint = self.constraints[action1_idx, action2_idx]
+                diff = abs(prev_constraint - self.current_demo_constraints[i,j])
+                if diff > 0:
+                    if action1 in new_action_list:
+                        self.constraints[self.action_map[action1], self.action_map[action2]] = 1
+                    else:
+                        self.constraints[self.action_map[action1], self.action_map[action2]] = 0
+
+        # conflicting_constraints = self.constraints - self.current_demo_constraints
+        # # print self.constraints
+        # # print self.current_demo_constraints
+        # conflicting_constraints_index = np.where(np.abs(conflicting_constraints) == 1)
+        # for diff_index in zip(conflicting_constraints_index[0], conflicting_constraints_index[1]):
+        #     self.constraints[diff_index] = 0
+        # # print self.constraints
+
+    def get_action_idx(self, action_name):
+        for action_idx, action in self.current_demo_action_map.items():
+            if action == action_name:
+                return action_idx
 
 if __name__ == '__main__':
     rospy.init_node('action_representation_learning_node')
---
+++ src/action_recognition/scripts/recognition.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+from keras.models import model_from_json
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from std_msgs.msg import Int16
+import tensorflow as tf
+import cv2
+
+import rospkg
+import rospy
+
+import numpy as np
+
+import threading
+
+from heapq import heappop
+import csv
+
+class task_data:
+    def __init__(self, start_frame, end_frame, task_number):
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+        self.task_number = task_number
+    def __str__(self):
+        return (str(self.start_frame) + ' ' + str(self.end_frame) + ' ' + str(self.task_number))
+    def __gt__(self, other):
+        return self.start_frame > other.start_frame
+    def __lt__(self, other):
+        return self.start_frame < other.start_frame
+    def __eq__(self, other):
+        return self.start_frame == other.start_frame
+
+class ActionRecognition(object):
+
+    def __init__(self, web_cam_topic, action_detected_topic, action_validation_topic, is_learning):
+        # define cv bridge to perform conversion from ros image to cv2 image
+        self.bridge = CvBridge()
+
+        # define the size of the video (height, width, depth)
+        self.count = 0
+        self.height = 406
+        self.width = 306
+        self.depth = 10
+        self.frames = np.zeros((self.width, self.height, self.depth, 3))
+
+        # retrieve the action recognition package path
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path('action_recognition')
+
+        # load the model architecture and the model weights
+        json_file = open(package_path+'/resources/action_3dcnnmodel.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.model = model_from_json(loaded_model_json)
+        self.model.load_weights(package_path+"/resources/action_3dcnnmodel-gpu.hd5")
+        self.model._make_predict_function()
+        print("Loaded model from disk")
+        self.graph = tf.get_default_graph()
+
+        # define the subscriber and publisher
+        rospy.Subscriber(web_cam_topic, Image, self.action_recognition_callback)
+
+        # used for multithreading purpose
+        self.recognizing_action = False
+        self.recognition_thread = None
+
+        # depending on whether it is performing learning or validation, publish
+        # the detected action to relevant topic
+        self.learning = is_learning
+
+        if self.learning:
+            self.detected_action_pub = rospy.Publisher(action_detected_topic, Int16, queue_size=10)
+        else:
+            self.detected_action_pub = rospy.Publisher(action_validation_topic, Int16, queue_size=10)
+
+        # read the csv files to get the action labels for validation the action representation model
+        # with the MP-II dataset
+        # self.csv_path = package_path+"/resources/detectionGroundtruth-1-0_excel.xlsx"
+        # self.get_ground_truth_data()
+
+
+    def get_ground_truth_data(self):
+        ground_truth_data = dict()
+        with open(self.csv_path) as f_obj:
+            reader = csv.reader(f_obj)
+            for row in reader:
+                task = task_data(int(row[2]), int(row[3]), int(row[4])-1)
+                frame_array = ground_truth_data.get(row[1], None)
+                if frame_array == None:
+                    frame_array = []
+                frame_array.append(task)
+                ground_truth_data[row[1]] = frame_array
+        self.ground_truth_data = ground_truth_data
+
+    def action_recognition_callback(self, data):
+        ## hardcoded data
+
+        if self.learning:
+            array_1 = np.array([0,1,2,3,-1])
+            array_2 = np.array([2,1,0,1,-1])
+            # array_3 = np.array([4,5,0,1,2,1,6,1,-1])
+            # array_4 = np.array([4,0,1,2,3,1,6,5,-1])
+            for val in array_1:
+                self.detected_action_pub.publish(val)
+            for val in array_2:
+                self.detected_action_pub.publish(val)
+            # for val in array_3:
+            #     self.detected_action_pub.publish(val)
+            # for val in array_4:
+            #     self.detected_action_pub.publish(val)
+        else:
+            array_5 = np.array([7,3,1,2,0,4,5,6,-1])
+            for val in array_5:
+                self.detected_action_pub.publish(val)
+
+        ## data from camera feed
+
+        # if self.recognizing_action:
+        #     return
+        # data = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+        # data = cv2.resize(data, (self.height, self.width))
+        # data = np.asarray(data)
+        # data = data.astype(float)
+        # self.frames[:,:,self.count,:] = data
+        # if self.count < self.depth - 1:
+        #     print "generating frames"
+        #     self.count += 1
+        # else:
+        #     self.recognizing_action = True
+        #     self.recognition_thread = threading.Thread(target=self.action_recognition,
+        #                                                args=(np.array(self.frames),))
+        #     self.recognition_thread.start()
+        #     self.count = 0
+
+        # data from MP-II dataset
+        #publish_action_labels('s08-d02-cam-002')
+
+
+    def action_recognition(self, frames):
+        clip = frames[np.newaxis]
+        print "calling action recognition"
+        with self.graph.as_default():
+            action_class = self.model.predict_classes(clip)
+            print action_class
+        self.detected_action_pub.publish(action_class)
+        self.recognizing_action = False
+        self.recognition_thread = None
+
+    def publish_action_labels(self, video_name):
+        video_ground_truth = self.ground_truth_data[video_name]
+        for _ in range(len(video_ground_truth)):
+            task = heappop(video_ground_truth)
+            print task
+            self.detected_action_pub.publish(task.task_number)
+
+if __name__ == '__main__':
+    rospy.init_node('action_recognition_node')
+    camera_topic = rospy.get_param('~camera_topic')
+    action_detected_topic = rospy.get_param('~action_detected_topic')
+    action_validation_topic = rospy.get_param('~action_validation_topic')
+    is_learning = rospy.get_param('~learning_topic')
+    action_recognition = ActionRecognition(camera_topic, action_detected_topic, action_validation_topic, is_learning)
+    rospy.spin()
